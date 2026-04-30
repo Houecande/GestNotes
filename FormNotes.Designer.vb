76c5e5045e2116c8f2e764c79ead91022d0d75d5
Armand Blade <armandhouecande9@gmail.com>
2026-04-30 09:54:48 +0100

    Design du FormNotes

--- FormNotes.Designer.vb
+++ FormNotes.Designer.vb
@@ -29,7 +29,39 @@ Partial Class FormNotes
         Me.btnCalculer = New System.Windows.Forms.Button()
         Me.btnEnregistrer = New System.Windows.Forms.Button()
         Me.pnlCorps = New System.Windows.Forms.Panel()
+        Me.pnlInfo = New System.Windows.Forms.Panel()
+        Me.lblInfoClasse = New System.Windows.Forms.Label()
+        Me.lblInfoMatiere = New System.Windows.Forms.Label()
+        Me.lblInfoFormule = New System.Windows.Forms.Label()
+        Me.lblInfoSaisies = New System.Windows.Forms.Label()
+        Me.dgvNotes = New System.Windows.Forms.DataGridView()
+        Me.colNumero = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.colNomEtudiant = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.colInterro = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.colDevoir = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.colMoyenne = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.pnlStats = New System.Windows.Forms.Panel()
+        Me.pnlStatMoyClasse = New System.Windows.Forms.Panel()
+        Me.pnlStatMin = New System.Windows.Forms.Panel()
+        Me.pnlStatMax = New System.Windows.Forms.Panel()
+        Me.pnlStatSaisies = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
         Me.pnlToolbar.SuspendLayout()
+        Me.pnlCorps.SuspendLayout()
+        Me.pnlInfo.SuspendLayout()
+        CType(Me.dgvNotes, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.pnlStats.SuspendLayout()
+        Me.pnlStatMoyClasse.SuspendLayout()
+        Me.pnlStatMin.SuspendLayout()
+        Me.pnlStatMax.SuspendLayout()
+        Me.pnlStatSaisies.SuspendLayout()
         Me.SuspendLayout()
         '
         'pnlToolbar
@@ -107,12 +139,259 @@ Partial Class FormNotes
         '
         'pnlCorps
         '
+        Me.pnlCorps.Controls.Add(Me.pnlStats)
+        Me.pnlCorps.Controls.Add(Me.dgvNotes)
+        Me.pnlCorps.Controls.Add(Me.lblInfoFormule)
+        Me.pnlCorps.Controls.Add(Me.pnlInfo)
         Me.pnlCorps.Dock = System.Windows.Forms.DockStyle.Fill
         Me.pnlCorps.Location = New System.Drawing.Point(0, 44)
         Me.pnlCorps.Name = "pnlCorps"
         Me.pnlCorps.Padding = New System.Windows.Forms.Padding(10)
         Me.pnlCorps.Size = New System.Drawing.Size(933, 475)
         Me.pnlCorps.TabIndex = 1
+        '
+        'pnlInfo
+        '
+        Me.pnlInfo.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.pnlInfo.BackColor = System.Drawing.Color.White
+        Me.pnlInfo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.pnlInfo.Controls.Add(Me.lblInfoSaisies)
+        Me.pnlInfo.Controls.Add(Me.lblInfoMatiere)
+        Me.pnlInfo.Controls.Add(Me.lblInfoClasse)
+        Me.pnlInfo.Location = New System.Drawing.Point(0, 0)
+        Me.pnlInfo.Name = "pnlInfo"
+        Me.pnlInfo.Size = New System.Drawing.Size(933, 36)
+        Me.pnlInfo.TabIndex = 0
+        '
+        'lblInfoClasse
+        '
+        Me.lblInfoClasse.AutoSize = True
+        Me.lblInfoClasse.ForeColor = System.Drawing.Color.FromArgb(CType(CType(12, Byte), Integer), CType(CType(68, Byte), Integer), CType(CType(124, Byte), Integer))
+        Me.lblInfoClasse.Location = New System.Drawing.Point(10, 10)
+        Me.lblInfoClasse.Name = "lblInfoClasse"
+        Me.lblInfoClasse.Size = New System.Drawing.Size(61, 15)
+        Me.lblInfoClasse.TabIndex = 0
+        Me.lblInfoClasse.Text = "Classe : —"
+        '
+        'lblInfoMatiere
+        '
+        Me.lblInfoMatiere.AutoSize = True
+        Me.lblInfoMatiere.ForeColor = System.Drawing.Color.FromArgb(CType(CType(12, Byte), Integer), CType(CType(68, Byte), Integer), CType(CType(124, Byte), Integer))
+        Me.lblInfoMatiere.Location = New System.Drawing.Point(160, 10)
+        Me.lblInfoMatiere.Name = "lblInfoMatiere"
+        Me.lblInfoMatiere.Size = New System.Drawing.Size(68, 15)
+        Me.lblInfoMatiere.TabIndex = 1
+        Me.lblInfoMatiere.Text = "Matière : —"
+        '
+        'lblInfoFormule
+        '
+        Me.lblInfoFormule.AutoSize = True
+        Me.lblInfoFormule.ForeColor = System.Drawing.Color.FromArgb(CType(CType(136, Byte), Integer), CType(CType(135, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.lblInfoFormule.Location = New System.Drawing.Point(320, 10)
+        Me.lblInfoFormule.Name = "lblInfoFormule"
+        Me.lblInfoFormule.Size = New System.Drawing.Size(241, 15)
+        Me.lblInfoFormule.TabIndex = 1
+        Me.lblInfoFormule.Text = "Formule : Moy = 30%×Interro + 70%×Devoir"
+        '
+        'lblInfoSaisies
+        '
+        Me.lblInfoSaisies.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lblInfoSaisies.AutoSize = True
+        Me.lblInfoSaisies.ForeColor = System.Drawing.Color.FromArgb(CType(CType(29, Byte), Integer), CType(CType(158, Byte), Integer), CType(CType(117, Byte), Integer))
+        Me.lblInfoSaisies.Location = New System.Drawing.Point(680, 10)
+        Me.lblInfoSaisies.Name = "lblInfoSaisies"
+        Me.lblInfoSaisies.Size = New System.Drawing.Size(60, 15)
+        Me.lblInfoSaisies.TabIndex = 2
+        Me.lblInfoSaisies.Text = "0/0 saisies"
+        '
+        'dgvNotes
+        '
+        Me.dgvNotes.AllowUserToAddRows = False
+        Me.dgvNotes.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.dgvNotes.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.dgvNotes.BackgroundColor = System.Drawing.Color.White
+        Me.dgvNotes.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.dgvNotes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvNotes.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.colNumero, Me.colNomEtudiant, Me.colInterro, Me.colDevoir, Me.colMoyenne})
+        Me.dgvNotes.Location = New System.Drawing.Point(0, 46)
+        Me.dgvNotes.MultiSelect = False
+        Me.dgvNotes.Name = "dgvNotes"
+        Me.dgvNotes.RowHeadersVisible = False
+        Me.dgvNotes.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect
+        Me.dgvNotes.Size = New System.Drawing.Size(240, 150)
+        Me.dgvNotes.TabIndex = 2
+        '
+        'colNumero
+        '
+        Me.colNumero.FillWeight = 8.0!
+        Me.colNumero.HeaderText = "N°"
+        Me.colNumero.Name = "colNumero"
+        Me.colNumero.ReadOnly = True
+        '
+        'colNomEtudiant
+        '
+        Me.colNomEtudiant.FillWeight = 35.0!
+        Me.colNomEtudiant.HeaderText = "Nom & Prénom"
+        Me.colNomEtudiant.Name = "colNomEtudiant"
+        Me.colNomEtudiant.ReadOnly = True
+        '
+        'colInterro
+        '
+        Me.colInterro.FillWeight = 20.0!
+        Me.colInterro.HeaderText = "Interro /20"
+        Me.colInterro.Name = "colInterro"
+        '
+        'colDevoir
+        '
+        Me.colDevoir.FillWeight = 20.0!
+        Me.colDevoir.HeaderText = "Devoir /20"
+        Me.colDevoir.Name = "colDevoir"
+        '
+        'colMoyenne
+        '
+        Me.colMoyenne.FillWeight = 17.0!
+        Me.colMoyenne.HeaderText = "Moyenne"
+        Me.colMoyenne.Name = "colMoyenne"
+        Me.colMoyenne.ReadOnly = True
+        '
+        'pnlStats
+        '
+        Me.pnlStats.Controls.Add(Me.pnlStatMax)
+        Me.pnlStats.Controls.Add(Me.pnlStatSaisies)
+        Me.pnlStats.Controls.Add(Me.pnlStatMin)
+        Me.pnlStats.Controls.Add(Me.pnlStatMoyClasse)
+        Me.pnlStats.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.pnlStats.Location = New System.Drawing.Point(10, 395)
+        Me.pnlStats.Name = "pnlStats"
+        Me.pnlStats.Size = New System.Drawing.Size(913, 70)
+        Me.pnlStats.TabIndex = 3
+        '
+        'pnlStatMoyClasse
+        '
+        Me.pnlStatMoyClasse.BackColor = System.Drawing.Color.White
+        Me.pnlStatMoyClasse.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.pnlStatMoyClasse.Controls.Add(Me.Label2)
+        Me.pnlStatMoyClasse.Controls.Add(Me.Label1)
+        Me.pnlStatMoyClasse.Location = New System.Drawing.Point(22, 6)
+        Me.pnlStatMoyClasse.Name = "pnlStatMoyClasse"
+        Me.pnlStatMoyClasse.Size = New System.Drawing.Size(140, 58)
+        Me.pnlStatMoyClasse.TabIndex = 0
+        '
+        'pnlStatMin
+        '
+        Me.pnlStatMin.BackColor = System.Drawing.Color.White
+        Me.pnlStatMin.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.pnlStatMin.Controls.Add(Me.Label6)
+        Me.pnlStatMin.Controls.Add(Me.Label5)
+        Me.pnlStatMin.Location = New System.Drawing.Point(483, 6)
+        Me.pnlStatMin.Name = "pnlStatMin"
+        Me.pnlStatMin.Size = New System.Drawing.Size(140, 58)
+        Me.pnlStatMin.TabIndex = 1
+        '
+        'pnlStatMax
+        '
+        Me.pnlStatMax.BackColor = System.Drawing.Color.White
+        Me.pnlStatMax.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.pnlStatMax.Controls.Add(Me.Label4)
+        Me.pnlStatMax.Controls.Add(Me.Label3)
+        Me.pnlStatMax.Location = New System.Drawing.Point(255, 6)
+        Me.pnlStatMax.Name = "pnlStatMax"
+        Me.pnlStatMax.Size = New System.Drawing.Size(140, 58)
+        Me.pnlStatMax.TabIndex = 1
+        '
+        'pnlStatSaisies
+        '
+        Me.pnlStatSaisies.BackColor = System.Drawing.Color.White
+        Me.pnlStatSaisies.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.pnlStatSaisies.Controls.Add(Me.Label8)
+        Me.pnlStatSaisies.Controls.Add(Me.Label7)
+        Me.pnlStatSaisies.Location = New System.Drawing.Point(751, 6)
+        Me.pnlStatSaisies.Name = "pnlStatSaisies"
+        Me.pnlStatSaisies.Size = New System.Drawing.Size(140, 58)
+        Me.pnlStatSaisies.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(32, 14)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(77, 30)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(94, 32)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(40, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(42, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(77, 30)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Label3"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(54, 29)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(40, 13)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "Label4"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(33, 14)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(77, 30)
+        Me.Label5.TabIndex = 1
+        Me.Label5.Text = "Label5"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(57, 29)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(40, 13)
+        Me.Label6.TabIndex = 2
+        Me.Label6.Text = "Label6"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(37, 14)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(77, 30)
+        Me.Label7.TabIndex = 1
+        Me.Label7.Text = "Label7"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(57, 29)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(40, 13)
+        Me.Label8.TabIndex = 2
+        Me.Label8.Text = "Label8"
         '
         'FormNotes
         '
@@ -129,6 +408,20 @@ Partial Class FormNotes
         Me.Text = "Saisie des notes"
         Me.pnlToolbar.ResumeLayout(False)
         Me.pnlToolbar.PerformLayout()
+        Me.pnlCorps.ResumeLayout(False)
+        Me.pnlCorps.PerformLayout()
+        Me.pnlInfo.ResumeLayout(False)
+        Me.pnlInfo.PerformLayout()
+        CType(Me.dgvNotes, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.pnlStats.ResumeLayout(False)
+        Me.pnlStatMoyClasse.ResumeLayout(False)
+        Me.pnlStatMoyClasse.PerformLayout()
+        Me.pnlStatMin.ResumeLayout(False)
+        Me.pnlStatMin.PerformLayout()
+        Me.pnlStatMax.ResumeLayout(False)
+        Me.pnlStatMax.PerformLayout()
+        Me.pnlStatSaisies.ResumeLayout(False)
+        Me.pnlStatSaisies.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
@@ -140,4 +433,28 @@ Partial Class FormNotes
     Friend WithEvents cmbMatiere As ComboBox
     Friend WithEvents btnEnregistrer As Button
     Friend WithEvents pnlCorps As Panel
+    Friend WithEvents pnlInfo As Panel
+    Friend WithEvents lblInfoClasse As Label
+    Friend WithEvents lblInfoMatiere As Label
+    Friend WithEvents lblInfoSaisies As Label
+    Friend WithEvents lblInfoFormule As Label
+    Friend WithEvents dgvNotes As DataGridView
+    Friend WithEvents colNumero As DataGridViewTextBoxColumn
+    Friend WithEvents colNomEtudiant As DataGridViewTextBoxColumn
+    Friend WithEvents colInterro As DataGridViewTextBoxColumn
+    Friend WithEvents colDevoir As DataGridViewTextBoxColumn
+    Friend WithEvents colMoyenne As DataGridViewTextBoxColumn
+    Friend WithEvents pnlStats As Panel
+    Friend WithEvents pnlStatMoyClasse As Panel
+    Friend WithEvents pnlStatMax As Panel
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents pnlStatSaisies As Panel
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents pnlStatMin As Panel
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
 End Class
--- FormNotes.vb
+++ FormNotes.vb
@@ -1,3 +1,9 @@
 ﻿Public Class FormNotes
+    Private Sub FlowLayoutPanel1_Paint(sender As Object, e As PaintEventArgs)
 
+    End Sub
+
+    Private Sub btnCalculer_Click(sender As Object, e As EventArgs) Handles btnCalculer.Click
+
+    End Sub
 End Class
